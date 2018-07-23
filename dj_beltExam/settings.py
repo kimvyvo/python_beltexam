ad3caeccb73ff3d118ad4db1bb765a3587b96ded
Kim Vy Vo <kimvy@kims-mbp.attlocal.net>
2018-07-22 22:49:21 -0700

    second commit

--- dj_beltExam/settings.py
+++ dj_beltExam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4sluop$w138xt0(jsw%kz6+6f52gmgs#s5k-*ny9x%!-80nxb^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.219.253.137']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
